7baecba9c0fcc742ecd74012f78a734f955fc507
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-05-08 11:09:45 +0300

    more testing

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(
 
 app.use(function(req, res, next) {
   if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host')+ "/app/", req.url].join());
+      return res.redirect(['https://', req.get('Host /app/'), req.url].join());
   }
   return next();
 });
