e96100a4c38f935f20ac99c0bd13926ba0a008bc
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-05-08 10:54:11 +0300

    getting there

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(
 
 app.use(function(req, res, next) {
   if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+      return res.redirect(['https://', req.get('Host'), req.url+"/app"].join(''));
   }
   return next();
 });
